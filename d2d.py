14b1957cc25ef4ab44b72c7b48f340601f845520
Jonas Palm <palmuu@gmail.com>
2015-02-26 16:07:49 +0100

    db wrapper

--- d2d.py
+++ d2d.py
@@ -1,4 +1,5 @@
 from flask import g
+import random
 
 def query_db(query, args=(), one=False):
   cur = g.db.execute(query, args)
@@ -6,23 +7,39 @@ def query_db(query, args=(), one=False):
   cur.close()
   return (rv[0] if rv else None) if one else rv
 
-# returns an list of dictionaries
+# returns an list of tuples
 def contracts(email):
-  return [{}] # all contracts where email = buyer or seller
+  return query_db('SELECT * FROM contracts WHERE buyer = ? OR seller = ?',
+      [email, email])
+
+# pending contracts for buyer with email
+def pending_contracts(email):
+  return query_db('SELECT * FROM contracts WHERE buyer = ? AND paid_at ISNULL',
+      [email])
 
 # creates a contract
 # returns the id of the new contract
 def create_contract(seller, buyer, pickup_addr, brn, ban, delivery_addr):
-  return 0
+  # TODO: Check for unqiueness
+  gen_id = random.randrange(1000000, 9999999)
+  g.db.execute('INSERT OR IGNORE INTO users VALUES (?)', [seller])
+  g.db.execute('INSERT OR IGNORE INTO users VALUES (?)', [buyer])
+  g.db.execute('INSERT INTO contracts(id, seller, buyer, pickup_addr, brn, ban, delivery_addr)' + \
+      ' VALUES (?, ?, ?, ?, ?, ?, ?)', [gen_id, seller, buyer, pickup_addr, brn, ban, delivery_addr])
+  g.db.commit()
+  return gen_id
 
 # returns all packages for contract id
-def packages_for_contract(id):
-  return [{}]
+def packages_for_contract(contractId):
+  return query_db('SELECT * FROM packages WHERE contract = ?', [contractId])
 
 # adds a package to a contract
-# returns id of the new package
+# returns nothing 
 def add_package_to_contract(contractId, price, desc, height, width, length, weight):
-  return 0
+  g.db.execute('INSERT INTO packages (contract, price, desc, height, width, length, weight)' + \
+      ' VALUES (?, ?, ?, ?, ?, ?, ?)', [contractId, price, desc, height, width, length, weight])
+  g.db.commit()
+  return 
 
 # sign a contract - this marks the contract
 # as signed by the seller and creates a 
@@ -30,6 +47,10 @@ def add_package_to_contract(contractId, price, desc, height, width, length, weig
 # packages included in the shipment
 # returns nothing
 def sign_contract(contractId):
+  g.db.execute('UPDATE contracts SET signed_at = datetime() WHERE id = ?', [contractId])
+  g.db.execute('INSERT INTO shipments (contract, delivery_price) VALUES ' + \
+      '(?, (SELECT SUM(width*height*length/1000000)*1000 FROM packages WHERE contract = ?))', [contractId, contractId])
+  g.db.commit()
   return
 
 # marks a contract as paid
@@ -37,31 +58,51 @@ def sign_contract(contractId):
 # returns nothing
 def pay_contract(contractId):
   # set paid_at column to datetime() for contractId=id
+  g.db.execute('UPDATE contracts SET paid_at = datetime() WHERE id = ?', [contractId])
+  g.db.commit()
   return
+
+# return shipment details for contract id
+def shipment(contractId):
+  return query_db('SELECT * FROM shipments WHERE contract = ?', [contractId])
 
 # assigns a driver to the shipment for
 # contractId. brn and ban are the drivers
 # bank routing och bank account numbers.
 # returns driver id
 def assign_driver_to_delivery(contractId, brn, ban):
-  return 0
+  cur = g.db.cursor()
+  cur.execute('INSERT INTO drivers VALUES (NULL, ?, ?)', [brn, ban])
+  driverId = cur.lastrowid
+  cur.execute('UPDATE shipments SET driver = ?, assigned_at = datetime() ' + \
+      'WHERE contract = ?', [driverId, contractId])
+  g.db.commit()
+  return driverId
 
 # Driver picks up delivery with contractId
 # and marks the time in the shipments table
 def pickup_delivery(contractId, driverId):
+  g.db.execute('UPDATE shipments SET pickup_at = datetime() WHERE contract = ?', [contractId])
+  g.db.commit()
   return
 
 # Driver drops off package at the buyer
 # and marks the time in the shipments table
 def dropoff_delivery(contractId, driverId):
+  g.db.execute('UPDATE shipments SET dropoff_at = datetime() WHERE contract = ?', [contractId])
+  g.db.commit()
   return
 
 # Buyer confirms and verifies the delivery
 # and marks the time in the shipments table
 def confirm_delivery(contractId):
+  g.db.execute('UPDATE shipments SET confirmed_at = datetime() WHERE contract = ?', [contractId])
+  g.db.commit()
   return
 
 # Contract is settled with the seller and
 # money is transfered to the seller's bank
 def settle_contract(contractId):
+  g.db.execute('UPDATE contracts SET settled_at = datetime() WHERE id = ?', [contractId])
+  g.db.commit()
   return
